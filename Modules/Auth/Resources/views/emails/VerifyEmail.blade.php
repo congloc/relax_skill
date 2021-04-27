d35b2aa1ebfe290acab183507f4bde722ea445b5
duoc0880 <ngocduoc@vutatech.vn>
2021-04-27 17:32:17 +0700

    fix text blade

--- Modules/Auth/Resources/views/emails/VerifyEmail.blade.php
+++ Modules/Auth/Resources/views/emails/VerifyEmail.blade.php
@@ -13,7 +13,7 @@ Code below to verify your account.
 <div class="w-100">
     <a href="{{ url('api/auth/verify-email?token='.$code) }}" target="_blank">{{ url('api/auth/verify-email?token='.$code) }}</a>
 </div>  --}}
-If you did not request a password reset, please contact us at support@relaxx.me
+If you did not operating this, please contact us at support@relaxx.me
 
 Thanks,<br>
 {{ config('app.name') }}
