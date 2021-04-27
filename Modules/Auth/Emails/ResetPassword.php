7492418870f277e4cb8a509caf9242832dffcfae
duoc0880 <ngocduoc@vutatech.vn>
2021-04-27 22:25:08 +0700

    fix email

--- Modules/Auth/Emails/ResetPassword.php
+++ Modules/Auth/Emails/ResetPassword.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class VerifyPassword extends Mailable
+class ResetPassword extends Mailable
 {
     use Queueable, SerializesModels;
 
