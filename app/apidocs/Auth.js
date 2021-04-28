79ff145c45115192c803f1f9c6f79e755d73493f
duoc0880 <ngocduoc@vutatech.vn>
2021-04-28 15:22:20 +0700

    add profile

--- app/apidocs/Auth.js
+++ app/apidocs/Auth.js
@@ -117,32 +117,3 @@
         "message": 'Your password has been updated successful.',
     }
  */
-
-  /**
- * @api {get} /api/profile Get Profile
- * @apiName get profile
- * @apiGroup Profile
- * 
- * @apiParam  {String} Bear token Authorization
- * 
- * @apiSuccessExample {type} Success-Response:
-    {
-    "status": 200,
-    "message": "Get data is success.",
-    "data": {
-        "id": 17,
-        "name": "relax",
-        "email": "ngocduoc@vutatech.vn",
-        "username": "relax",
-        "email_verified_at": null,
-        "google2fa_secret": null,
-        "google2fa_status": 0,
-        "status": 1,
-        "type": "1",
-        "ref": "xxxxxx",
-        "code": 543697,
-        "created_at": "2021-04-28T07:16:26.000000Z",
-        "updated_at": "2021-04-28T07:16:26.000000Z"
-    }
-   }
- */
