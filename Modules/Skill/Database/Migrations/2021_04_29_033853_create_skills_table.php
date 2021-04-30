cca2ef7427257a005241b962b249b1003f66b63b
Admin <congloc@vutatech.vn>
2021-04-30 17:17:51 +0700

    module_skill

--- Modules/Skill/Database/Migrations/2021_04_29_033853_create_skills_table.php
+++ Modules/Skill/Database/Migrations/2021_04_29_033853_create_skills_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWardsTable extends Migration
+class CreateSkillsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateWardsTable extends Migration
      */
     public function up()
     {
-        Schema::create('wards', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
-            $table->bigInteger('province_id')->unsigned();            
-            $table->timestamps();
         });
     }
 
@@ -28,8 +26,6 @@ class CreateWardsTable extends Migration
      */
     public function down()
     {
-        Schema::table('wards', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('skills');
     }
 }
---
+++ Modules/Skill/Http/Controllers/SkillController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Skill\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Modules\Skill\Models\Skill;
+use Illuminate\Http\JsonResponse as HttpJsonResponse;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Renderable
+     */
+    public function index()
+    {
+        $skill = Skill::all();
+        
+        return response()->json($skill, HttpJsonResponse::HTTP_OK);
+    }
+}
